37afaef7858ff333b52b7b1f181c92fec9f85788
karth2k <karthikmuthukumar05@gmail.com>
2021-09-28 15:02:33 -0700

    Finished Quiz Project

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -15,27 +15,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //this code adds one to the count value (allows score to increase by one)
     public void addcount(View view) {
         count = count+1;
     }
 
+    //the following lines of code call the functions defined further below
     public void endscore(View view){
         displayfinal();
     }
+
+    public void scoreset(View view){
+        resetscore();
+    }
+    //this function adds up the correct answers and provides the user with their final score
 
     private void displayfinal() {
         TextView textviewfinal = (TextView) findViewById(R.id.final_score);
         textviewfinal.setText("Final Score: " + count + "/7");
     }
 
-    public void countreset(View view){
-        resetscore();
-    }
-
+    //this function resets the score display to blank, so the quiz can be retaken
     private void resetscore() {
-        count = 0;
+        count =0;
         TextView textviewfinal = (TextView) findViewById(R.id.final_score);
         textviewfinal.setText("Final Score: ");
     }
 
 }
+
